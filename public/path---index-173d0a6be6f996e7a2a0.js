7b8e2730b273858b25caf52e6e1cd18351b9900f
Rohit Mishra <Rohit>
2018-02-08 19:20:06 +0530

    Created one new branch for gatsby

---
+++ public/path---index-173d0a6be6f996e7a2a0.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{485:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Blah Blah Fake Title"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-173d0a6be6f996e7a2a0.js.map
---
+++ src/pages/my-files.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>My Site's Files</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>relativePath</th>
+            <th>prettySize</th>
+            <th>extension</th>
+            <th>birthTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allSitePage.edges.map(({ node }, index) =>
+            <tr key={index}>
+              <td>
+                {node.componentPath}
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query MyFilesQuery {
+  allMarkdownRemark{
+   edges {
+     node {
+       id
+     }
+   }
+ }
+  }
+`
